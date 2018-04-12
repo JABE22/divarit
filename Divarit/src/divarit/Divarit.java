2ef6b2c5096158990634046f9468b1fe29d8e815
Jarno Matarmaa <Jarno Matarmaa@153.1.238.112>
2018-04-12 14:41:03 +0300

    Kayttoliittyma lisätty

--- Divarit/src/divarit/Divarit.java
+++ Divarit/src/divarit/Divarit.java
@@ -10,6 +10,8 @@ public class Divarit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Kayttoliittyma kl = new Kayttoliittyma();
+        kl.kaynnista();
         // TODO code application logic here
     }
     
