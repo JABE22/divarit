4bb4782ef356a8dec649eb9e84f23ddcd3c2daf2
Jarno Matarmaa <Jarno Matarmaa@153.1.238.112>
2018-04-12 14:57:02 +0300

    Kayttoliittyma lisätty

--- Divarit/src/divarit/Kayttoliittyma.java
+++ Divarit/src/divarit/Kayttoliittyma.java
@@ -5,6 +5,8 @@
  */
 package divarit;
 
+import divarit.helpers.In;
+
 /**
  *
  * @author Jarno Matarmaa
@@ -13,7 +15,26 @@ public class Kayttoliittyma {
 
     void kaynnista() {
         
+        String[] syote = komentorivi();
+        
+        System.out.println("** Divarit **");
+        
+        if (syote != null && syote.length > 0) {
+        } else {
+            String komento = syote[0];
+        }
+        
         
     }
     
+    public static String[] komentorivi() {
+        System.out.print(">");
+        String syote = In.readString();
+        String[] osat = syote.split(" ", 2);
+
+        return osat;
+    }
+    
 }
+
+
