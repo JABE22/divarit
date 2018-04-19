ce7505eb9e0c510df49c5cfe9ddf2fba71f43c65
Jarno <matarmaa.jarno.o@student.uta.fi>
2018-04-19 22:29:18 +0300

    Sisennyksiä kyselyihin

--- meta/R1_Hakukyselyt.sql
+++ meta/R1_Hakukyselyt.sql
@@ -9,6 +9,6 @@ WHERE LOWER(nimi) LIKE ? OR LOWER(tyyppi) LIKE ? OR LOWER(luokka) LIKE ?
 UNION
 SELECT t.isbn, t.nimi, t.kuvaus, t.luokka, t.tyyppi
 FROM keskusdivari.teos t
-INNER JOIN keskusdivari.teosten_tekijat ktt ON t.isbn = ktt.teos_isbn
-INNER JOIN keskusdivari.tekija kt ON ktt.tekija_id = kt.id
+    INNER JOIN keskusdivari.teosten_tekijat ktt ON t.isbn = ktt.teos_isbn
+    INNER JOIN keskusdivari.tekija kt ON ktt.tekija_id = kt.id
 WHERE LOWER(kt.etunimi) LIKE ? OR LOWER(kt.sukunimi) LIKE ?;
