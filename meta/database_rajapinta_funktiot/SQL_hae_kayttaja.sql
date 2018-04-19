18d3c3ee06d347bf62bb92c31e8b596e32f5c070
Xaaria <xaaria@users.noreply.github.com>
2018-04-19 14:49:04 +0300

    käyttäjähaku-funktio
    ....

---
+++ meta/database_rajapinta_funktiot/SQL_hae_kayttaja.sql
@@ -0,0 +1,24 @@
+/*
+	
+
+*/
+
+SET SCHEMA 'keskusdivari';
+
+-- DROP FUNCTION hea_kayttaja CASCADE;
+
+CREATE OR REPLACE FUNCTION hea_kayttaja(param_user_email varchar(255))
+RETURNS TABLE(
+	email varchar(255),
+	etunimi varchar(20),
+	sukunimi varchar(60),
+	osoite varchar(100),
+	puhelin varchar(100),
+	div_yllapitaja boolean
+)
+AS $$
+	SELECT email, etunimi, sukunimi, osoite, 
+	CASE WHEN puhelin IS NULL THEN 'ei_annettu' ELSE puhelin END, div_yllapitaja
+	FROM keskusdivari.kayttaja
+	WHERE email = param_user_email;
+$$ LANGUAGE SQL;
