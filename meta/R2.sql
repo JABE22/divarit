0db43835298aa3c40ba2fb98d9ac2bb871f0d305
Xaaria <xaaria@users.noreply.github.com>
2018-04-21 15:02:50 +0300

    Raportti 2 (!)
    Raportti 2 uudistettu siten, että tyhjäarvo kuuluu luokkaan 'Luokittelematon' raportissa.
    
    Tehty kyselystä kutsuttava funktio ja lisätty dokumentaatio api.txt:hen

--- meta/R2.sql
+++ meta/R2.sql
@@ -2,12 +2,13 @@
 -- RYHMITTELE MYYNNISSÄ OLEVAT teokset niiden LUOKAN mukaan.
 -- Anna luokkien teosten kokonaismyyntihinta JA keskihinta
 
-SELECT teos.luokka, -- teos.isbn, teos.nimi, kappale.id
+SELECT COALESCE(teos.luokka, 'Luokittelematon') as teos, -- teos.isbn, teos.nimi, kappale.id
 SUM(kappale.hinta) AS kokonaismyyntihinta, 
 AVG(kappale.hinta) AS keskihinta 
 FROM kappale
 
 INNER JOIN teos ON kappale.teos_isbn=teos.isbn
 
-WHERE kappale.tila=0 -- 0 = vapaa
-GROUP BY(teos.luokka);
+WHERE kappale.tila=0 -- 0 = vapaa. HUOM: MYYNNISSÄ OLEVAT KAPPALEET
+GROUP BY(teos.luokka)
+ORDER BY luokka ASC;
