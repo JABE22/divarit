805dec4887719cdef4bb071ddd90ed63544c3d81
JABE22 <37688643+JABE22@users.noreply.github.com>
2018-04-23 01:53:51 +0300

    Tilakriteeri lisätty

--- meta/Rajapintafunktiot/FUNC_5_ostoskorin_tuotteet.sql
+++ meta/Rajapintafunktiot/FUNC_5_ostoskorin_tuotteet.sql
@@ -1,11 +1,11 @@
-SET SCHEMA 'keskusdivari';
+﻿SET SCHEMA 'keskusdivari';
 -- DROP FUNCTION tilauksen_teokset CASCADE;
 
 CREATE OR REPLACE FUNCTION ostoskorin_tuotteet(tid int)
 RETURNS TABLE(
 	tilaus_id int,
 	kappale_id int,
-    teosnimi varchar(60)
+        teosnimi varchar(60)
 )
 AS $$
 	SELECT t.id, kappale_id, nimi  
@@ -13,6 +13,7 @@ AS $$
         INNER JOIN keskusdivari.ostoskori ok ON t.id = ok.tilaus_id
         INNER JOIN keskusdivari.kappale k ON ok.kappale_id = k.id
         INNER JOIN keskusdivari.teos ON k.teos_isbn = teos.isbn
-    WHERE t.id = $1 AND t.tila <> 2
+    WHERE t.id = $1 AND t.tila = 1
+
     ORDER BY k.id;
 $$ LANGUAGE SQL;
