d88f46ad4788d287da754f7549d67db7d1ae42b7
Xaaria <xaaria@users.noreply.github.com>
2018-04-23 13:36:22 +0300

    Funktio 7 (raportti 3) JOIN
    F7 Joinaa nyt myös oikein kappale-relaatioon. Koodi vanhentunut, korjattu!

--- meta/Rajapintafunktiot/FUNC_7_raportti.sql
+++ meta/Rajapintafunktiot/FUNC_7_raportti.sql
@@ -13,11 +13,12 @@ AS $$
 	LEFT JOIN (
 		SELECT kayttaja.email as k_e, COUNT(DISTINCT(kappale.teos_isbn)) as eri_kirjojen_lkm_vuoden_sis FROM keskusdivari.tilaus
 			INNER JOIN keskusdivari.ostoskori ON tilaus.id=ostoskori.tilaus_id
-			INNER JOIN keskusdivari.kappale ON ostoskori.kappale_id=kappale.id
+			INNER JOIN keskusdivari.kappale ON ostoskori.kappale_id=kappale.id AND ostoskori.divari_nimi=kappale.divari_nimi
 			RIGHT JOIN keskusdivari.kayttaja ON tilaus.kayttaja_email=kayttaja.email
 			WHERE
 			tilaus.tila=2 AND -- 2 = valmis tilaus
-			tilaus.pvm >= CURRENT_DATE - '1 year'::interval 
+			-- tilaus.pvm >= CURRENT_DATE - '1 year'::interval 
+			EXTRACT(YEAR FROM NOW()::date)-1 = EXTRACT(YEAR FROM tilaus.pvm)
 			GROUP BY(kayttaja.email)
 	) as laskenta ON kayttaja.email=laskenta.k_e
 	ORDER BY kayttaja.email ASC;
